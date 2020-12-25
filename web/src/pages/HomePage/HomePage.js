8823f774f3cffbebaf9ac2641159a21ba9e7c419
Phil Lackey <plackey@costar.com>
2020-12-24 23:34:49 -0600

    created posts scaffold and migration

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,22 +1,9 @@
-import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const HomePage = () => {
   return (
     <>
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-            <li>
-              <Link to={routes.testing()}>Testing</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>Home Page!</main>
+      <BlogLayout>Home Page!</BlogLayout>
     </>
   )
 }
--- web/src/pages/TestingPage/TestingPage.js
+++ web/src/pages/TestingPage/TestingPage.js
@@ -1,16 +1,13 @@
 import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const TestingPage = () => {
   return (
     <>
-      <h1>TestingPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/TestingPage/TestingPage.js</code>
-      </p>
-      <p>
+      <BlogLayout>
         My default route is named <code>testing</code>, link to me with `
         <Link to={routes.testing()}>Testing</Link>`
-      </p>
+      </BlogLayout>
     </>
   )
 }
